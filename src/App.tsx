4542a0b4ff800232e20762d7149e3039bdaba98a
Jorge Repossi <jorgerepossi1980@gmail.com>
2024-04-29 22:47:58 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className='flex'>
+      <p> Crear un aside que contenga el menu</p>
+      <p> Header Bar que muestre el usuario y login</p>
+      <p> mostrar una "tabla" que contenga los links que se puedan ir navegando</p>
+    </div>
+  )
+}
+
+
+export default App
